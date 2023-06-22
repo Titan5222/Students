cf1fb39f93a9578eac7cf7e58cb0c4032f60bcef
Titans5222 <jajudiya@my.centennialcollege.ca>
2023-06-21 23:27:19 -0400

    Student Rest api CRUD

--- config/databse.config.js
+++ config/databse.config.js
@@ -1,6 +1,4 @@
-// module.exports = {
-//     url:"mongodb+srv://urvishpatelaws:5JOrS935QrHo4KB7@cluster0.csowkf6.mongodb.net/?retryWrites=true&w=majority"
-// }
+
 module.exports = {
     url:"mongodb+srv://titans:T7xnOT2dOtvcl7be@cluster0.vjjae4j.mongodb.net/?retryWrites=true&w=majority"
 }
